7e4809e91895dd3f76d5053f204e3d134066a4d2
bandikut <golimarmusic@gmail.com>
2021-05-28 13:39:00 +0200

    Initial commit

---
+++ src/main/java/pontsystems/eu/linkcollection/LinkCollectionApplication.java
@@ -0,0 +1,13 @@
+package pontsystems.eu.linkcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pontsystems/eu/linkcollection/LinkCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package pontsystems.eu.linkcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
